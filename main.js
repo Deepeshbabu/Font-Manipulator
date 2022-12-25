e18f933114353ab81382be251caad150f9f5cdda
Deepeshbabu <104300644+Deepeshbabu@users.noreply.github.com>
2022-12-25 11:44:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nx = 0 ;
+ny = 0 ;
+lw = 0 ;
+rw = 0 ;
+d = 0 ;
+
 function setup() 
 {
     video = createCapture(VIDEO) ;
@@ -5,7 +11,7 @@ function setup()
     video.position(100,100);
 
     canvas = createCanvas(550,550) ;
-    canvas.position(800,115) ;
+    canvas.position(800,110) ;
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -18,13 +24,25 @@ function modelLoaded()
 
 function draw() 
 {
-    background('#969A97') ;
+    background('aliceblue') ;
+    fill("black") ;
+    textSize(d) ;
+    text("H.Deepesh Babu",floor(nx), floor(ny)) ;
+    document.getElementById("result").innerHTML = "Font Size Of The Text Will Be : " + d + " px" ;
 }
 
 function gotPoses(results) 
 {
-    if(results.length = 0)
+    if(results.length > 0)
     {
         console.log(results);
+        nx = results[0].pose.nose.x ;
+        ny = results[0].pose.nose.y ;
+        console.log("Nose X = " + nx + "Nose Y = " + ny);
+
+        lw = results[0].pose.leftWrist.x ;
+        rw = results[0].pose.rightWrist.x ;
+        d = floor(lw) - floor(rw) ;
+        console.log("d = " + d + "lw = " + lw + "rw = " + rw);
     }
 }
